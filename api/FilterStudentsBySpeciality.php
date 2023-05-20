7af6411fb47fe885a8c2b81c975dd18c02a3f6af
Safwa9amar <hassanih97@gmail.com>
2023-05-20 03:03:00 +0200

    up

--- api/FilterStudentsBySpeciality.php
+++ api/FilterStudentsBySpeciality.php
@@ -2,5 +2,5 @@
 include_once '../app/config/config.php';
 include_once '../app/controllers/dataBaseController.php';
 $db = new DatabaseController();
-$students = $db->getRowsByParam('students', 'speciality_id', $_GET['id']);
+$students = $db->getRowsByParam('stagaires', 'speciality_id', $_GET['id']);
 echo json_encode($students);
